9291872fd92b3c1c15ed8f2c4473fc6a32f1dc7b
哲の王 <creatcodebuild@github.com>
2020-05-15 22:31:23 +0000

    implemented close operation

--- maga/src/channel.ts
+++ maga/src/channel.ts
@@ -1,16 +1,18 @@
 export class Channel<T> {
-
+    private closed: boolean = false;
     popActions = [];
     putActions = [];
 
     put(ele: T) {
+        if (this.closed) {
+            throw new Error('can not put to a closed channel');
+        }
         // if no pop action awaiting
         if (this.popActions.length === 0) {
+            if (this.putActions.length >= 1) {
+                throw new Error('all promise asleep');
+            }
             return new Promise((resolve) => {
-                console.log('put promise');
-                if (this.putActions.length >= 1) {
-                    throw new Error('all promise asleep');
-                }
                 this.putActions.push([resolve, ele]);
             })
         } else {
@@ -23,12 +25,14 @@ export class Channel<T> {
     }
 
     pop(): Promise<T> {
+        if (this.closed) {
+            return undefined;
+        }
         if (this.putActions.length === 0) {
+            if (this.popActions.length >= 1) {
+                throw new Error('pop all promise asleep');
+            }
             return new Promise((resolve) => {
-                console.log('pop promise');
-                if (this.popActions.length >= 1) {
-                    throw new Error('pop all promise asleep');
-                }
                 this.popActions.push(resolve);
             })
         } else {
@@ -38,6 +42,22 @@ export class Channel<T> {
                 resolve(ele)
             });
             // return ele;
+        }
+    }
+
+    // put to a closed channel throws an error
+    // pop from a closed channel returns undefined
+    // close a closed channel throws an error
+    close() {
+        if (this.closed) {
+            throw Error('can not close a channel twice');
+        }
+        this.closed = true;
+    }
+
+    async*[Symbol.asyncIterator]() {
+        while (!this.closed) {
+            yield await this.pop();
         }
     }
 }
--- maga/src/channel_test.ts
+++ maga/src/channel_test.ts
@@ -27,4 +27,52 @@ describe("", async () => {
         await t2;
     })
 
+    it("supports iteration protocol", async () => {
+        let c = chan<number>();
+        let task1 = async () => {
+            let i = 0;
+            while (1) {
+                await c.put(++i);
+                await c.put(++i);
+            }
+        }
+        let task2 = async () => {
+            let data = [];
+            let i = 0;
+            for await (let x of c) {
+                i++
+                data.push(x);
+                if (i === 10) {
+                    break
+                }
+            }
+            deepStrictEqual(data, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        }
+        let t1 = task1();
+        let t2 = task2();
+        await t2;
+    })
+
+    it("closes", async () => {
+        let c = chan<number>();
+        let task1 = async () => {
+            await c.put(1);
+            await c.put(2);
+            c.close();
+        }
+        let task2 = async () => {
+            let data = [];
+            let i = 0;
+            for await (let x of c) {
+                i++
+                data.push(x);
+            }
+            deepStrictEqual(data, [1, 2])
+        }
+        let t1 = task1();
+        let t2 = task2();
+        await t1;
+        await t2;
+    })
+
 });
