dc2c99474a680bf42eebfccf06cf1993435b032f
哲の王 <albertcodelab@gmail.com>
2020-05-20 02:55:29 +0000

    push dist

--- csp/dist/csp.js
+++ csp/dist/csp.js
@@ -1,46 +1,30 @@
-interface PopperOnResolver<T> {
-    (ele: { value: undefined, done: true } | { value: T, done: false }): void
-}
-
-export interface Channel<T> {
-    put(ele: T): Promise<void>
-    pop(): Promise<T | undefined>
-    close()
-    closed(): boolean
-}
-
-export interface SelectableChannel<T> extends Channel<T> {
-    ready(i: number): Promise<number>
-}
-
-export interface IterableChannel<T> extends Channel<T> {
-    [Symbol.asyncIterator]: AsyncIterator<T, T>
-}
-
-export class UnbufferredChannel<T> implements SelectableChannel<T> {
-    private _closed: boolean = false;
-    private popActions: PopperOnResolver<T>[] = [];
-    putActions: Array<{ resolver: Function, ele: T }> = [];
-    readyListener: { resolve: Function, i: number }[] = [];
-
-    put(ele: T): Promise<void> {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.select = exports.chan = exports.UnbufferredChannel = void 0;
+class UnbufferredChannel {
+    constructor() {
+        this._closed = false;
+        this.popActions = [];
+        this.putActions = [];
+        this.readyListener = [];
+    }
+    put(ele) {
         if (this._closed) {
             throw new Error('can not put to a closed channel');
         }
-
         if (this.readyListener.length > 0) {
             for (let { resolve, i } of this.readyListener) {
                 resolve(i);
             }
             this.readyListener = [];
         }
-
         // if no pop action awaiting
         if (this.popActions.length === 0) {
             return new Promise((resolve) => {
                 this.putActions.push({ resolver: resolve, ele });
-            })
-        } else {
+            });
+        }
+        else {
             return new Promise((resolve) => {
                 let onPop = this.popActions.shift();
                 if (onPop === undefined) {
@@ -50,40 +34,37 @@ export class UnbufferredChannel<T> implements SelectableChannel<T> {
                 resolve();
             });
         }
-
     }
-
     // checks if a channel is ready to be read but dooes not read it
     // it returns only after the channel is ready
-    async ready(i: number): Promise<number> {
+    async ready(i) {
         if (this.putActions.length > 0 || this._closed) {
             return i;
-        } else {
+        }
+        else {
             return new Promise((resolve) => {
                 this.readyListener.push({ resolve, i });
-            })
+            });
         }
     }
-
-    async pop(): Promise<T | undefined> {
+    async pop() {
         let next = this.next();
         if (next instanceof Promise) {
             return (await next).value;
         }
         return next.value;
     }
-
-    next(): Promise<{ value: T, done: false } | { value: undefined, done: true }> | { value: undefined, done: true } {
+    next() {
         if (this._closed) {
             return { value: undefined, done: true };
         }
-
         // console.log('poppers', this.putActions, this.popActions);
         if (this.putActions.length === 0) {
             return new Promise((resolve, reject) => {
                 this.popActions.push(resolve);
-            })
-        } else {
+            });
+        }
+        else {
             return new Promise((resolve) => {
                 let putAction = this.putActions.shift();
                 if (putAction === undefined) {
@@ -95,7 +76,6 @@ export class UnbufferredChannel<T> implements SelectableChannel<T> {
             });
         }
     }
-
     // put to a closed channel throws an error
     // pop from a closed channel returns undefined
     // close a closed channel throws an error
@@ -119,35 +99,41 @@ export class UnbufferredChannel<T> implements SelectableChannel<T> {
         }
         this._closed = true;
     }
-
     closed() {
         return this._closed;
     }
-
-    [Symbol.asyncIterator]() {
-        return this;
+    async *[Symbol.asyncIterator]() {
+        while (!this._closed) {
+            let next = this.next();
+            if (next instanceof Promise) {
+                let r = (await next);
+                if (r.done) {
+                    return r.value;
+                }
+                yield r.value;
+            }
+            else {
+                if (next.done) {
+                    return next.value;
+                }
+                yield next.value;
+            }
+        }
+        return undefined;
     }
 }
-
-export function chan<T>() {
-    return new UnbufferredChannel<T>();
+exports.UnbufferredChannel = UnbufferredChannel;
+function chan() {
+    return new UnbufferredChannel();
 }
-
-interface onSelect<T> {
-    (ele: T | undefined): Promise<any>
-}
-
-interface DefaultCase<T> {
-    (): Promise<T>
-}
-
+exports.chan = chan;
 // https://stackoverflow.com/questions/37021194/how-are-golang-select-statements-implemented
-export async function select<T>(channels: [UnbufferredChannel<T>, onSelect<T>][], defaultCase?: DefaultCase<T>): Promise<any> {
-    let promises: Promise<number>[] = channels.map(([c, func], i) => {
+async function select(channels, defaultCase) {
+    let promises = channels.map(([c, func], i) => {
         return c.ready(i);
-    })
+    });
     if (defaultCase) {
-        promises = promises.concat([Promise.resolve(promises.length)])
+        promises = promises.concat([Promise.resolve(promises.length)]);
     }
     let i = await Promise.race(promises);
     if (defaultCase && i === promises.length - 1) {
@@ -156,3 +142,4 @@ export async function select<T>(channels: [UnbufferredChannel<T>, onSelect<T>][]
     let ele = await channels[i][0].pop();
     return await channels[i][1](ele);
 }
+exports.select = select;
